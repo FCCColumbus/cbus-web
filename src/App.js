9c5ce17e32d338bda4f9578dbb030f376fbaae59
amyngb <amyngbedinghaus@gmail.com>
2022-08-20 14:03:44 -0400

    testing eslint

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import "./App.css";
+import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello World!!! </h1>
-      <a aria-hidden> should fail linting </a>
       {/* <a aria-hidden> should fail linting </a> */}
     </div>
   );
