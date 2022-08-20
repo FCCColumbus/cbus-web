49043caa032d37c9a2933035b7734bbf5f6705e9
amyngb <amyngbedinghaus@gmail.com>
2022-08-20 14:57:01 -0400

    disable ESLint rules that will conflict with prettier

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import "./App.css";
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World!!! </h1>
+      <h1>Hello World!!</h1>
     </div>
   );
 }
