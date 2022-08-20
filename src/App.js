3fd81684794188a0472889bff4b28d409ec81308
amyngb <amyngbedinghaus@gmail.com>
2022-08-20 14:10:14 -0400

    attempt to run lint-staged only once

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <h1>Hello World!!! </h1>
+      <a aria-hidden> should fail linting </a>
     </div>
   );
 }
