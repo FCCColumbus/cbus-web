cabc2e2671ea9fedca3dacb8b329ecba5603a3c7
elsalvadordali <tloochich@icloud.com>
2022-09-22 16:45:51 -0400

    two small tests as a test

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
      <img alt="substitute logo" src="/temp_logo.png"/>
 
       <a href="https://www.netlify.com"> <img src="https://www.netlify.com/v3/img/components/netlify-color-accent.svg" alt="Deploys by Netlify" /> </a>
-
+      
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,19 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("renders Hello World!", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Hello World/i);
-  expect(linkElement).toBeInTheDocument();
-});
+test("has a description of what the group is", () => {
+  render(<App />)
+
+  const div = screen.getByText(/is a /i)
+  expect(div).toBeInTheDocument()
+  expect(div).not.toBeEmptyDOMElement()
+})
+
+test("has link to freecodecamp website", () => {
+  const { container } = render(<App />)
+
+  const link = container.querySelector('a href="https://www.freecodecamp.org/"')
+  expect(link).toBeInTheDocument()
+})
+
+
