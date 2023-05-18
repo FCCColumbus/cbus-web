415c32b89722942b6eff23f46c6d36c4570c6293
Jed Duffey <jedduffey@gmail.com>
2023-05-18 17:43:15 -0400

    Reformat and remove one test

--- src/App.test.js
+++ src/App.test.js
@@ -2,41 +2,33 @@ import { render } from "@testing-library/react";
 import App from "./App";
 
 describe("App", () => {
-  describe("renders external links", () => {
+  describe("renders links to external sites", () => {
     it.each([
-      ['Discord homepage', 'discord.gg'],
       ['freeCodeCamp.org homepage', 'freecodecamp.org'],
+      ['Discord homepage', 'discord.gg'],
       ['FCCColumbus Github profile', 'github.com/FCCColumbus'],
+      ['FCCColumbus Github contributors', 'github.com/FCCColumbus/cbus-web/graphs/contributors'],
     ])(`%s`, (_, expected) => {
       const { container } = render(<App />);
       const anchorElements = container.querySelectorAll('a');
 
       const actual = Object.values(anchorElements)
-        .map(element => element.attributes.href.value)
+        .map(anchorElement => anchorElement.attributes.href.value)
         .some(value => value.includes(expected));
 
       expect(actual).toBeTruthy();
     });
   });
-});
 
-xtest("has a description of what the group is", () => {
-  const { container } = render(<App />);
+  test("has an Events section", () => {
+    const { container } = render(<App />);
+    const event = container.querySelector('#events');
+    expect(event).toBeInTheDocument();
+  });
 
-  const div = container.querySelector('.description');
-  expect(div).toBeInTheDocument();
-  expect(div).not.toBeEmptyDOMElement();
-});
-
-xtest("has events", () => {
-  const { container } = render(<App />);
-
-  const event = container.querySelector('.event');
-  expect(event).toBeInTheDocument();
-});
-
-xtest("has footer", () => {
-  const { container } = render(<App />);
-  const footer = container.querySelector('footer');
-  expect(footer).toBeInTheDocument();
+  test("has an About section", () => {
+    const { container } = render(<App />);
+    const about = container.querySelector('#about');
+    expect(about).toBeInTheDocument();
+  });
 });
