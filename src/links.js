241a55190cf885cfa84249ae4ed3974e3eaaf03a
Jed Duffey <jedduffey@gmail.com>
2023-05-18 17:28:32 -0400

    About link goes to About section

--- src/links.js
+++ src/links.js
@@ -1,6 +1,6 @@
 export default [
     { href: '/', text: 'Home' },
-    { href: '#about', text: 'About' },
+    { href: '#about-section', text: 'About' },
     { href: 'https://www.freecodecamp.org/', text: 'FreeCodeCamp' },
     { href: 'https://discord.gg', text: 'Discord' },
     { href: 'https://github.com/FCCColumbus', text: 'GitHub' },
