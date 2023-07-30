23c50078fcc56935e49b571525798a0c0e71460f
Michael McBride <Michaelmcbridefilms@gmail.com>
2023-07-30 14:55:07 -0400

    update discord nav link

--- src/test/components/Footer.test.js
+++ src/test/components/Footer.test.js
@@ -1,10 +1,17 @@
-import renderer from "react-test-renderer";
+import renderer from 'react-test-renderer';
 
-import Footer from "../../components/Footer";
+import Footer from '../../components/Footer';
+import { render, screen } from '@testing-library/react';
 
-describe("Footer", () => {
-  it("renders", () => {
+describe('Footer', () => {
+  it('renders', () => {
     const view = renderer.create(<Footer />);
     expect(view).toMatchSnapshot();
+  });
+
+  it('displays "Discord" as the third item', () => {
+    render(<Footer />);
+    const thirdItem = screen.getByText('Discord');
+    expect(thirdItem).toBeInTheDocument();
   });
 });
--- src/test/components/Header.test.js
+++ src/test/components/Header.test.js
@@ -1,10 +1,17 @@
-import renderer from "react-test-renderer";
+import renderer from 'react-test-renderer';
 
-import Header from "../../components/Header";
+import Header from '../../components/Header';
+import { render, screen } from '@testing-library/react';
 
-describe("Header", () => {
-  it("renders", () => {
+describe('Header', () => {
+  it('renders', () => {
     const view = renderer.create(<Header />);
     expect(view).toMatchSnapshot();
+  });
+
+  it('displays "Discord" as the third item', () => {
+    render(<Header />);
+    const thirdItem = screen.getByText('Discord');
+    expect(thirdItem).toBeInTheDocument();
   });
 });
