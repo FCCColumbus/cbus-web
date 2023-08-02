16dce3745832c6ea42448ce4b757afe3541c7693
Michael McBride <Michaelmcbridefilms@gmail.com>
2023-08-02 11:38:48 -0400

    fix typo in links.js

--- src/links.js
+++ src/links.js
@@ -5,5 +5,5 @@ export default [
   { href: 'https://discord.com/invite/EXehPVnBYz', text: 'Discord' },
   { href: 'https://github.com/FCCColumbus', text: 'GitHub' },
   { href: '#events', text: 'Events' },
-  { href: 'https://github.com/FCCColumbus/cbus-web/graphs/contributors', text:'Contributors' },
+  { href: 'https://github.com/FCCColumbus/cbus-web/graphs/contributors', text: 'Contributors' },
 ];
