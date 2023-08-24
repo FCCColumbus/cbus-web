01ec22099392d5286069d54f971d35a47f03d6f4
John Riley <github@readysetagile.com>
2023-08-24 15:10:31 -0400

    Further simplified the app test
    - removed the redundant footer test
    - simplified the app test to use a RegEx to test the component order

--- src/test/App.test.js
+++ src/test/App.test.js
@@ -1,63 +1,36 @@
 import { render, screen } from "@testing-library/react";
 
 import App from "../App"
-
 jest.mock('../components/Header', () => {
-  return () => <div>Header Mock</div>
+  return () => <div>HeaderMock</div>
 });
 jest.mock('../components/Splash', () => {
-  return () => <div>Splash Mock</div>
+  return () => <div>SplashMock</div>
 });
 jest.mock('../components/About', () => {
-  return () => <div>About Mock</div>
+  return () => <div>AboutMock</div>
 });
 jest.mock('../components/Events', () => {
-  return () => <div>Events Mock</div>
+  return () => <div>EventsMock</div>
 });
 jest.mock('../components/Members', () => {
-  return () => <div>Members Mock</div>
+  return () => <div>MembersMock</div>
 });
 jest.mock('../components/Footer', () => {
-  return () => (
-    <div>
-      <a href="freecodecamp.org">freeCodeCamp.org homepage</a>
-      <a href="discord.com/invite/EXehPVnBYz">Discord homepage</a>
-      <a href="github.com/FCCColumbus">FCCColumbus Github profile</a>
-      <a href="github.com/FCCColumbus/cbus-web/graphs/contributors">FCCColumbus Github contributors</a>
-    </div>
-  );
+  return () => <div>FooterMock</div>
 });
 
 describe("App", () => {
-  describe("renders links to external sites", () => {
-    it.each([
-      ['freeCodeCamp.org homepage', 'freecodecamp.org'],
-      ['Discord homepage', 'discord.com/invite/EXehPVnBYz'],
-      ['FCCColumbus Github profile', 'github.com/FCCColumbus'],
-      ['FCCColumbus Github contributors', 'github.com/FCCColumbus/cbus-web/graphs/contributors'],
-    ])(`%s`, (_, expected) => {
-      const { container } = render(<App />);
-      const anchorElements = container.querySelectorAll('a');
-
-      const actual = Object.values(anchorElements)
-        .map(anchorElement => anchorElement.attributes.href.value)
-        .some(value => value.includes(expected));
-
-      expect(actual).toBeTruthy();
-    });
-  });
-
   test("renders App components in order", () => {
+    //first, render the app as we mocked it
     render(<App />)
-    expect(screen.getByText('Header Mock')).toBeInTheDocument(); 
-    expect(screen.getByText('Splash Mock')).toBeInTheDocument();  
-    expect(screen.getByText('About Mock')).toBeInTheDocument();  
-    expect(screen.getByText('Events Mock')).toBeInTheDocument();
-    expect(screen.getByAltText('code on a screen. mostly css')).toBeInTheDocument();
-    expect(screen.getByText('Members Mock')).toBeInTheDocument();  
-    expect(screen.getByText("freeCodeCamp.org homepage")).toBeInTheDocument();
-    expect(screen.getByText("Discord homepage")).toBeInTheDocument();
-    expect(screen.getByText("FCCColumbus Github profile")).toBeInTheDocument();
-    expect(screen.getByText("FCCColumbus Github contributors")).toBeInTheDocument();
+
+    //make sure the Header component is there
+    expect(screen.getByText('HeaderMock')).toBeInTheDocument(); 
+    //the Header's parent should be the 
+    const appnode = screen.getByText('HeaderMock').parentElement;
+
+    expect(appnode.outerHTML).toMatch(/HeaderMock{1,1}[\s\S]*SplashMock{1,1}[\s\S]*AboutMock{1,1}[\s\S]*EventsMock{1,1}[\s\S]*MembersMock{1,1}[\s\S]*FooterMock{1,1}/s);
   });
+
 });
